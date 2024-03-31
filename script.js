777c520f452c27601571b853998b85372383ce3c
houssamcha <157011410+houssamcha@users.noreply.github.com>
2024-03-31 23:48:40 +0000

    Creating the search contact function

---
+++ script.js
@@ -0,0 +1,16 @@
+function searchContacts() {
+      var input, filter, contactList, contacts, contact, i, txtValue;
+      input = document.getElementById("searchInput");
+      filter = input.value.toUpperCase();
+      contactList = document.getElementById("contactList");
+      contacts = contactList.getElementsByClassName("contact");
+      for (i = 0; i < contacts.length; i++) {
+        contact = contacts[i].getElementsByClassName("contact-details")[0];
+        txtValue = contact.getElementsByTagName("h2")[0].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          contacts[i].style.display = "";
+        } else {
+          contacts[i].style.display = "none";
+        }
+      }
+    }
